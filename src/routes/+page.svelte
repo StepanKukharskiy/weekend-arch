c4014da6f84b23b49827b2c878c8b7f11f4c66b7
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2024-06-07 11:06:03 +0000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,71 @@
+<main>
+	<h1>Weekend Arch</h1>
+	<p>Короткие интенсивы для архитекторов по</p>
+	<ul>
+		<li>параметрическому дизайну</li>
+		<li>генеративному дизайну</li>
+		<li>веб дизайну</li>
+		<li>гейм дизайну</li>
+	</ul>
+	<p>С использованием Rhino, Grasshopper, Blender, HTML, CSS, JavaScript.</p>
+
+	<div class="imageContainer"></div>
+
+	<h2>Интенсив 1. Воксель</h2>
+	<p>
+		В ходе интенсива попробуем разобраться в методологии создания воксельных форм с использованием
+		Grasshopper, текстурирвоания и подготовки ассетов в Blender, а также создания интерактивной
+		модели в браузере с получившейся моделью.
+	</p>
+	<p>Интенсив состоит из 3 частей:</p>
+	<ul>
+		<li>основы моделирвоания в Grasshopper</li>
+		<li>основы текстурирования и подготовки ассетов для сипользования в веб-проектах в Blender</li>
+		<li>
+			создание интерактивной сцены с использованием языка программирования JavaScript и библиотеки
+			Three.js на платформе Kodiia.com
+		</li>
+	</ul>
+	<p>
+		Интенсив проходит в онлайн формате, ориентирован на новичков и подойдет для всех, кто хочет
+		разобарться с основами работы в Grasshopper, Blender, HTML, CSS, JavaScript
+	</p>
+	<p>Требования к участникам:</p>
+	<ul>
+		<li>наличие ноутбука или ПК с выходом в интернет</li>
+		<li>установленные Rhino, Grasshopper, Blender</li>
+		<li>регистрация на платформе Kodiia.com</li>
+	</ul>
+	<p>Продолжительность 2 дня по 1,5 часа.</p>
+	<p>Стоимость участия 5000 руб.</p>
+	<button>Записаться</button>
+</main>
+<footer>
+	<p>ИП Черейская Алина Игоревна</p>
+	<p>политика конфиденциальности</p>
+	<p>публичная оферта</p>
+</footer>
+
+<style>
+    :global(body){
+        margin: 0;
+        background: radial-gradient(circle, rgb(224, 223, 223) 1px, #f9f9f9 1px);
+        background-size: 20px 20px;
+    }
+    main{
+        padding: 10px;
+    }
+    footer{
+        padding: 10px;
+        background: black;
+        color: #f9f9f9;
+    }
+	.imageContainer {
+		width: 100%;
+		height: 80svh;
+		background-image: url('https://images.adsttc.com/media/images/5bec/31c3/08a5/e5a5/8c00/0693/large_jpg/6.jpg?1542205883');
+		background-position: center;
+		background-size: cover;
+		background-repeat: no-repeat;
+	}
+</style>
