5a9de0ccd21be7c8b38f4bdcdf7012ac94b12035
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2024-06-08 20:04:07 +0000

    layout fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,134 @@
 <main>
-	<h1>Weekend Arch</h1>
-	<p>Короткие интенсивы для архитекторов по</p>
+	<h1>SA lab :: ARCH WEEKEND</h1>
+	<div class="imageContainer"></div>
+	<div class="running-text-container">
+		<h3 class="running-text">Rhino + Grasshopper + Blender + HTML + CSS + JavaScript</h3>
+	</div>
+	<div class="gridContainer">
+		<div class="gridSmallCard">
+			<h3>29 и 30 июня</h3>
+		</div>
+		<div class="gridSmallCard">
+			<h3>online</h3>
+		</div>
+		<div class="gridSmallCard">
+			<h3>7500₽</h3>
+		</div>
+		<div class="gridSmallCard">
+			<button class="registerButton">Записаться</button>
+		</div>
+
+		<div class="gridBigCard">
+			<h3>ARCH WEEKEND —</h3>
+			<p>
+				это смелый прыжок в мир алгоритмического проектирования для архитекторов и дизайнеров,
+				который научит ориентироваться в технологиях в контексте стремительно развивающегося мира.
+				Мы предлагаем быстрое погружение в генеративный дизайн, цифровые миры, искусственный
+				интеллект и геймдизайн.
+				<br /><br />Архитекторы SA lab придумали ARCH WEEKEND, потому что знают, что учиться новому
+				может быть <span style="text-decoration: line-through;">больно</span> весело. Более 10 лет
+				мы используем в архитектурной практике цифровые технологии, а последние 5 лет учим студентов
+				из 22 стран. Полученные навыки помогают шире взглянуть на архитектуру, выстроить собственный
+				профессиональный трек в российских и зарубежных компаниях.
+				<br /><br />Выходные нужны, чтобы смотреть сериалы или гулять по городу, мы предлагаем
+				встретиться онлайн.
+			</p>
+		</div>
+
+		<div class="gridSmallCard">
+			<h3>ARCHWEEKEND vol 1.0</h3>
+		</div>
+		<div class="gridSmallCard">
+			<p>В первый раз мы встретимся, чтобы:</p>
+			<ul>
+				<li>
+					рассмотреть принципы формообразования воксельной архитектуры на примере здания RED7 от
+					бюро MVRDV,
+				</li>
+				<li>создать модель с использованием Rhino+Grasshopper,</li>
+				<li>создать на ее основе ассет в Blender для использования в браузере,</li>
+				<li>
+					добавить немного магии с HTML + CSS + JavaScript, чтобы создать интерактивную сцену.
+				</li>
+			</ul>
+		</div>
+
+		<!-- <div class="gridBigCard">
+			<h3>Программа</h3>
+		</div> -->
+
+		<div class="gridSmallCard" style="justify-content: start; align-items: start;">
+			<h3>29 июня</h3>
+			<ul>
+				<li>Основы моделирования в Grasshopper</li>
+				<li>Моделирование воксельного здания</li>
+				<li>Основы подготовки ассетов и их текстурирования в Blender</li>
+				<li>Подготовка ассета воксельного здания</li>
+			</ul>
+		</div>
+
+		<div class="gridSmallCard" style="justify-content: start; align-items: start;">
+			<h3>30 июня</h3>
+			<ul>
+				<li>Основы работы с HTML+CSS+JavaScript</li>
+				<li>Основы работы с библиотекой Three.js</li>
+				<li>Создание интерактивной сцены, которая будет работать в браузере</li>
+			</ul>
+		</div>
+
+		<div class="gridBigCard">
+			<h3>Куратор :: Степан Кухарский</h3>
+			<p>
+				Всем привет! Меня зовут Степан. Я архитектор. Более 10 лет в составе команды SA lab я создаю
+				городские территории, здания, малые формы, а также виртуальные пространства и игры.
+				<br /><br />Я стараюсь делиться своим опытом и принимаю участие в ведущих международных
+				конференциях - DigitalFUTURES, CDRF, eCAADe.
+				<br /><br />Творческий процесс - это эксперимент. А образовательная деятельность -
+				возможность провести большее количество экспериментов с большой ценностью для всех
+				участников.
+			</p>
+		</div>
+
+		<div class="gridSmallCard" style="justify-content: start; align-items: start;">
+			<h3>TL;DR</h3>
+			<ul>
+				<li>29 и 30 июня по 2 часа каждый день.</li>
+				<li>Онлайн. Запись будет доступна участникам.</li>
+				<li>
+					Подойдет для всех, кто хочет разобраться с основами работы в Grasshopper, Blender, HTML,
+					CSS, JavaScript.
+				</li>
+			</ul>
+		</div>
+
+		<div class="gridSmallCard" style="justify-content: start; align-items: start;">
+			<h3>Технические требования</h3>
+			<ul>
+				<li>наличие ноутбука или ПК с выходом в интернет</li>
+				<li>установленные Rhino, Grasshopper, Blender</li>
+				<li>
+					регистрация на платформе <a href="https://kodiia.com" target="_blank">Kodiia.com</a>
+				</li>
+			</ul>
+		</div>
+
+		<div class="gridBigCard">
+			<button class="registerButton">Записаться</button>
+		</div>
+
+		<div class="gridBigCard">
+			<h3>Мы на связи</h3>
+		</div>
+
+		<div class="gridSmallCard">
+			<a href="mailto:hello@salab.org">hello@salab.org</a>
+		</div>
+		<div class="gridSmallCard">
+			<a href="https://t.me/SAlab_daily" target="_blank">@salab.daily</a>
+		</div>
+	</div>
+
+	<!-- <p>Короткие интенсивы для архитекторов по</p>
 	<ul>
 		<li>параметрическому дизайну</li>
 		<li>генеративному дизайну</li>
@@ -8,8 +136,6 @@
 		<li>гейм дизайну</li>
 	</ul>
 	<p>С использованием Rhino, Grasshopper, Blender, HTML, CSS, JavaScript.</p>
-
-	<div class="imageContainer"></div>
 
 	<h2>Интенсив 1. Воксель</h2>
 	<p>
@@ -37,8 +163,7 @@
 		<li>регистрация на платформе Kodiia.com</li>
 	</ul>
 	<p>Продолжительность 2 дня по 1,5 часа.</p>
-	<p>Стоимость участия 5000 руб.</p>
-	<button>Записаться</button>
+	<p>Стоимость участия 5000 руб.</p> -->
 </main>
 <footer>
 	<p>ИП Черейская Алина Игоревна</p>
@@ -47,19 +172,41 @@
 </footer>
 
 <style>
-    :global(body){
-        margin: 0;
-        background: radial-gradient(circle, rgb(224, 223, 223) 1px, #f9f9f9 1px);
-        background-size: 20px 20px;
-    }
-    main{
-        padding: 10px;
-    }
-    footer{
-        padding: 10px;
-        background: black;
-        color: #f9f9f9;
-    }
+	:global(body) {
+		width: 100vw;
+		margin: 0;
+		background: radial-gradient(circle, rgb(224, 223, 223) 1px, #f9f9f9 1px);
+		background-size: 20px 20px;
+		font-family: sans-serif;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	main {
+		padding: 0 10px;
+		width: 100%;
+		max-width: 800px;
+		font-weight: 300;
+		box-sizing: border-box;
+	}
+	h3 {
+		font-weight: 500;
+	}
+	a {
+		color: blue;
+	}
+	footer {
+		border: 0.5px solid blue;
+		width: calc(100% - 19px);
+		max-width: 800px;
+		padding: 0 10px;
+		background: #0000ff10;
+		box-sizing: border-box;
+		color: blue;
+		font-weight: 300;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
 	.imageContainer {
 		width: 100%;
 		height: 80svh;
@@ -67,5 +214,124 @@
 		background-position: center;
 		background-size: cover;
 		background-repeat: no-repeat;
+		border: 0.5px solid blue;
+		box-sizing: border-box;
+	}
+
+	.running-text-container {
+		border: 0.5px solid blue;
+		box-sizing: border-box;
+		white-space: nowrap;
+		text-transform: uppercase;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		color: blue;
+		background-color: #f9f9f9;
+		/* font-size: 30px; */
+	}
+
+	.running-text {
+		margin: 5px 0;
+		/* animation properties */
+		-moz-transform: translateX(100%);
+		-webkit-transform: translateX(100%);
+		transform: translateX(100%);
+
+		-moz-animation: my-animation 15s linear infinite;
+		-webkit-animation: my-animation 15s linear infinite;
+		animation: my-animation 15s linear infinite;
+	}
+
+	.gridContainer {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		/* color: blue; */
+	}
+	.gridSmallCard {
+		min-height: 80px;
+		padding: 10px;
+		border: 0.5px solid blue;
+		background-color: #f9f9f9;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.gridBigCard {
+		border: 0.51px solid blue;
+		padding: 10px;
+		grid-column: span 2;
+		background-color: #f9f9f9;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+	}
+	.gridContainer h3 {
+		margin: 0;
+		font-weight: 500;
+		color: blue;
+	}
+	.registerButton {
+		width: 100%;
+		height: 50px;
+		margin: 0;
+		border: none;
+		text-transform: uppercase;
+		font-size: 20px;
+		font-weight: 500;
+		background: #0000ff10;
+		color: blue;
+		cursor: pointer;
+		transition: all 0.25s;
+		padding: 10px;
+	}
+	.registerButton:hover {
+		background-color: blue;
+		color: #f9f9f9;
+	}
+
+	/* for Firefox */
+	@-moz-keyframes my-animation {
+		from {
+			-moz-transform: translateX(100%);
+		}
+		to {
+			-moz-transform: translateX(-100%);
+		}
+	}
+
+	/* for Chrome */
+	@-webkit-keyframes my-animation {
+		from {
+			-webkit-transform: translateX(100%);
+		}
+		to {
+			-webkit-transform: translateX(-100%);
+		}
+	}
+
+	@keyframes my-animation {
+		from {
+			-moz-transform: translateX(100%);
+			-webkit-transform: translateX(100%);
+			transform: translateX(100%);
+		}
+		to {
+			-moz-transform: translateX(-100%);
+			-webkit-transform: translateX(-100%);
+			transform: translateX(-100%);
+		}
+	}
+
+	@media screen and (max-width: 700px) {
+		.gridContainer {
+			display: grid;
+			grid-template-columns: 1fr;
+		}
+		.gridBigCard {
+			grid-column: span 1;
+			padding: 10px;
+		}
 	}
 </style>
