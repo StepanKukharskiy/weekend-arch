7c88fcbefe1020f263f1d7d7ffdc3761ee4eb377
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2024-06-09 17:52:57 +0000

    layout fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <main>
 	<h1>SA lab :: ARCH WEEKEND</h1>
 	<div class="imageContainer"></div>
-	<div class="running-text-container">
-		<h3 class="running-text">Rhino + Grasshopper + Blender + HTML + CSS + JavaScript</h3>
-	</div>
+
 	<div class="gridContainer">
+		<div class="gridBigCard">
+			<div class="running-text-container">
+				<h3 class="running-text">Rhino + Grasshopper + Blender + HTML + CSS + JavaScript</h3>
+			</div>
+		</div>
+
 		<div class="gridSmallCard">
 			<h3>29 и 30 июня</h3>
 		</div>
@@ -17,99 +21,117 @@
 		<div class="gridSmallCard">
 			<button class="registerButton">Записаться</button>
 		</div>
+	</div>
 
-		<div class="gridBigCard">
-			<h3>ARCH WEEKEND —</h3>
-			<p>
-				это смелый прыжок в мир алгоритмического проектирования для архитекторов и дизайнеров,
-				который научит ориентироваться в технологиях в контексте стремительно развивающегося мира.
-				Мы предлагаем быстрое погружение в генеративный дизайн, цифровые миры, искусственный
-				интеллект и геймдизайн.
-				<br /><br />Архитекторы SA lab придумали ARCH WEEKEND, потому что знают, что учиться новому
-				может быть <span style="text-decoration: line-through;">больно</span> весело. Более 10 лет
-				мы используем в архитектурной практике цифровые технологии, а последние 5 лет учим студентов
-				из 22 стран. Полученные навыки помогают шире взглянуть на архитектуру, выстроить собственный
-				профессиональный трек в российских и зарубежных компаниях.
-				<br /><br />Выходные нужны, чтобы смотреть сериалы или гулять по городу, мы предлагаем
-				встретиться онлайн.
-			</p>
+	<div class="gridContainer" style="border: none;">
+		<div class="gridBigCard" style="border: none; padding-top: 10px;">
+			<div class="gridCardContainer">
+				<h3>ARCH WEEKEND —</h3>
+				<p>
+					это смелый прыжок в мир алгоритмического проектирования для архитекторов и дизайнеров,
+					который научит ориентироваться в технологиях в контексте стремительно развивающегося мира.
+					Мы предлагаем быстрое погружение в генеративный дизайн, цифровые миры, искусственный
+					интеллект и геймдизайн.
+					<br /><br />Архитекторы SA lab придумали ARCH WEEKEND, потому что знают, что учиться
+					новому может быть <span style="text-decoration: line-through;">больно</span> весело. Более
+					10 лет мы используем в архитектурной практике цифровые технологии, а последние 5 лет учим
+					студентов из 22 стран. Полученные навыки помогают шире взглянуть на архитектуру, выстроить
+					собственный профессиональный трек в российских и зарубежных компаниях.
+					<br /><br />Выходные нужны, чтобы смотреть сериалы или гулять по городу, мы предлагаем
+					встретиться онлайн и немного покодить.
+				</p>
+			</div>
 		</div>
-
+	</div>
+	<div class="gridContainer">
 		<div class="gridSmallCard">
-			<h3>ARCHWEEKEND vol 1.0</h3>
+			<div class="gridCardContainer">
+				<h3>ARCHWEEKEND vol 1.0</h3>
+			</div>
 		</div>
 		<div class="gridSmallCard">
-			<p>В первый раз мы встретимся, чтобы:</p>
-			<ul>
-				<li>
-					рассмотреть принципы формообразования воксельной архитектуры на примере здания RED7 от
-					бюро MVRDV,
-				</li>
-				<li>создать модель с использованием Rhino+Grasshopper,</li>
-				<li>создать на ее основе ассет в Blender для использования в браузере,</li>
-				<li>
-					добавить немного магии с HTML + CSS + JavaScript, чтобы создать интерактивную сцену.
-				</li>
-			</ul>
+			<div class="gridCardContainer">
+				<p>В первый раз мы встретимся, чтобы:</p>
+				<ul>
+					<li>
+						рассмотреть принципы формообразования воксельной архитектуры на примере здания RED7 от
+						бюро MVRDV,
+					</li>
+					<li>создать модель с использованием Rhino+Grasshopper,</li>
+					<li>создать на ее основе ассет в Blender для использования в браузере,</li>
+					<li>
+						добавить немного магии с HTML + CSS + JavaScript, чтобы создать интерактивную сцену.
+					</li>
+				</ul>
+			</div>
 		</div>
 
-		<!-- <div class="gridBigCard">
-			<h3>Программа</h3>
-		</div> -->
+		<div class="gridSmallCard" style="justify-content: start; align-items: center;">
+			<div class="gridCardContainer">
+				<h3>29 июня</h3>
+				<ul>
+					<li>Основы моделирования в Grasshopper</li>
+					<li>Моделирование воксельного здания</li>
+					<li>Основы подготовки ассетов и их текстурирования в Blender</li>
+					<li>Подготовка ассета воксельного здания</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="gridSmallCard" style="justify-content: start; align-items: center;">
+			<div class="gridCardContainer">
+				<h3>30 июня</h3>
+				<ul>
+					<li>Основы работы с HTML+CSS+JavaScript</li>
+					<li>Основы работы с библиотекой Three.js</li>
+					<li>Создание интерактивной сцены, которая будет работать в браузере</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="gridContainer" style='border: none;'>
+		<div class="gridBigCard" style="border: none; padding-top: 10px;">
+			<div class="gridCardContainer">
+				<h3>Куратор :: Степан Кухарский</h3>
+				<p>
+					Всем привет! Меня зовут Степан. Я архитектор. Более 10 лет в составе команды SA lab я
+					создаю городские территории, здания, малые формы, а также виртуальные пространства и игры.
+					<br /><br />Я стараюсь делиться своим опытом и принимаю участие в ведущих международных
+					конференциях - DigitalFUTURES, CDRF, eCAADe.
+					<br /><br />Творческий процесс - это эксперимент. А образовательная деятельность -
+					возможность провести большее количество экспериментов с большой ценностью для всех
+					участников. Присоединяйтесь на наш ARCH WEEKEND!
+				</p>
+			</div>
+		</div>
+	</div>
+
+<div class='gridContainer'>
+		<div class="gridSmallCard" style="justify-content: start; align-items: start;">
+			<div class="gridCardContainer">
+				<h3>TL;DR</h3>
+				<ul>
+					<li>29 и 30 июня по 2 часа каждый день.</li>
+					<li>Онлайн. Запись будет доступна участникам.</li>
+					<li>
+						Подойдет для всех, кто хочет разобраться с основами работы в Grasshopper, Blender, HTML,
+						CSS, JavaScript.
+					</li>
+				</ul>
+			</div>
+		</div>
 
 		<div class="gridSmallCard" style="justify-content: start; align-items: start;">
-			<h3>29 июня</h3>
-			<ul>
-				<li>Основы моделирования в Grasshopper</li>
-				<li>Моделирование воксельного здания</li>
-				<li>Основы подготовки ассетов и их текстурирования в Blender</li>
-				<li>Подготовка ассета воксельного здания</li>
-			</ul>
-		</div>
-
-		<div class="gridSmallCard" style="justify-content: start; align-items: start;">
-			<h3>30 июня</h3>
-			<ul>
-				<li>Основы работы с HTML+CSS+JavaScript</li>
-				<li>Основы работы с библиотекой Three.js</li>
-				<li>Создание интерактивной сцены, которая будет работать в браузере</li>
-			</ul>
-		</div>
-
-		<div class="gridBigCard">
-			<h3>Куратор :: Степан Кухарский</h3>
-			<p>
-				Всем привет! Меня зовут Степан. Я архитектор. Более 10 лет в составе команды SA lab я создаю
-				городские территории, здания, малые формы, а также виртуальные пространства и игры.
-				<br /><br />Я стараюсь делиться своим опытом и принимаю участие в ведущих международных
-				конференциях - DigitalFUTURES, CDRF, eCAADe.
-				<br /><br />Творческий процесс - это эксперимент. А образовательная деятельность -
-				возможность провести большее количество экспериментов с большой ценностью для всех
-				участников.
-			</p>
-		</div>
-
-		<div class="gridSmallCard" style="justify-content: start; align-items: start;">
-			<h3>TL;DR</h3>
-			<ul>
-				<li>29 и 30 июня по 2 часа каждый день.</li>
-				<li>Онлайн. Запись будет доступна участникам.</li>
-				<li>
-					Подойдет для всех, кто хочет разобраться с основами работы в Grasshopper, Blender, HTML,
-					CSS, JavaScript.
-				</li>
-			</ul>
-		</div>
-
-		<div class="gridSmallCard" style="justify-content: start; align-items: start;">
-			<h3>Технические требования</h3>
-			<ul>
-				<li>наличие ноутбука или ПК с выходом в интернет</li>
-				<li>установленные Rhino, Grasshopper, Blender</li>
-				<li>
-					регистрация на платформе <a href="https://kodiia.com" target="_blank">Kodiia.com</a>
-				</li>
-			</ul>
+			<div class="gridCardContainer">
+				<h3>Технические требования</h3>
+				<ul>
+					<li>наличие ноутбука или ПК с выходом в интернет</li>
+					<li>установленные Rhino, Grasshopper, Blender</li>
+					<li>
+						регистрация на платформе <a href="https://kodiia.com" target="_blank">Kodiia.com</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 
 		<div class="gridBigCard">
@@ -117,7 +139,9 @@
 		</div>
 
 		<div class="gridBigCard">
-			<h3>Мы на связи</h3>
+			<div class="gridCardContainer">
+				<h3>Мы на связи:</h3>
+			</div>
 		</div>
 
 		<div class="gridSmallCard">
@@ -127,60 +151,26 @@
 			<a href="https://t.me/SAlab_daily" target="_blank">@salab.daily</a>
 		</div>
 	</div>
-
-	<!-- <p>Короткие интенсивы для архитекторов по</p>
-	<ul>
-		<li>параметрическому дизайну</li>
-		<li>генеративному дизайну</li>
-		<li>веб дизайну</li>
-		<li>гейм дизайну</li>
-	</ul>
-	<p>С использованием Rhino, Grasshopper, Blender, HTML, CSS, JavaScript.</p>
-
-	<h2>Интенсив 1. Воксель</h2>
-	<p>
-		В ходе интенсива попробуем разобраться в методологии создания воксельных форм с использованием
-		Grasshopper, текстурирвоания и подготовки ассетов в Blender, а также создания интерактивной
-		модели в браузере с получившейся моделью.
-	</p>
-	<p>Интенсив состоит из 3 частей:</p>
-	<ul>
-		<li>основы моделирвоания в Grasshopper</li>
-		<li>основы текстурирования и подготовки ассетов для сипользования в веб-проектах в Blender</li>
-		<li>
-			создание интерактивной сцены с использованием языка программирования JavaScript и библиотеки
-			Three.js на платформе Kodiia.com
-		</li>
-	</ul>
-	<p>
-		Интенсив проходит в онлайн формате, ориентирован на новичков и подойдет для всех, кто хочет
-		разобарться с основами работы в Grasshopper, Blender, HTML, CSS, JavaScript
-	</p>
-	<p>Требования к участникам:</p>
-	<ul>
-		<li>наличие ноутбука или ПК с выходом в интернет</li>
-		<li>установленные Rhino, Grasshopper, Blender</li>
-		<li>регистрация на платформе Kodiia.com</li>
-	</ul>
-	<p>Продолжительность 2 дня по 1,5 часа.</p>
-	<p>Стоимость участия 5000 руб.</p> -->
 </main>
 <footer>
-	<p>ИП Черейская Алина Игоревна</p>
-	<p>политика конфиденциальности</p>
-	<p>публичная оферта</p>
+	<div class="footerContainer">
+		<p>ИП Черейская Алина Игоревна</p>
+		<p>политика конфиденциальности</p>
+		<p>публичная оферта</p>
+	</div>
 </footer>
 
 <style>
 	:global(body) {
 		width: 100vw;
 		margin: 0;
-		background: radial-gradient(circle, rgb(224, 223, 223) 1px, #f9f9f9 1px);
+		background: radial-gradient(circle, #1a1a1a50 1px, #f9f9f9 1px);
 		background-size: 20px 20px;
 		font-family: sans-serif;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		font-size: 1.2rem;
 	}
 	main {
 		padding: 0 10px;
@@ -189,24 +179,19 @@
 		font-weight: 300;
 		box-sizing: border-box;
 	}
+	h1{
+		background-color: #f9f9f9;
+		margin: 0;
+		padding: 20px 0;
+	}
 	h3 {
+		font-size: 1.5rem;
 		font-weight: 500;
 	}
 	a {
 		color: blue;
 	}
-	footer {
-		border: 0.5px solid blue;
-		width: calc(100% - 19px);
-		max-width: 800px;
-		padding: 0 10px;
-		background: #0000ff10;
-		box-sizing: border-box;
-		color: blue;
-		font-weight: 300;
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-	}
+
 	.imageContainer {
 		width: 100%;
 		height: 80svh;
@@ -214,12 +199,12 @@
 		background-position: center;
 		background-size: cover;
 		background-repeat: no-repeat;
-		border: 0.5px solid blue;
+		/* border: 1px solid #1a1a1a; */
+		/* margin-bottom: 10px; */
 		box-sizing: border-box;
 	}
 
 	.running-text-container {
-		border: 0.5px solid blue;
 		box-sizing: border-box;
 		white-space: nowrap;
 		text-transform: uppercase;
@@ -245,12 +230,22 @@
 	.gridContainer {
 		display: grid;
 		grid-template-columns: 1fr 1fr;
+		border: 1px solid #1a1a1a;
+
 		/* color: blue; */
+	}
+	.gridCardContainer {
+		padding: 10px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 	}
 	.gridSmallCard {
 		min-height: 80px;
-		padding: 10px;
-		border: 0.5px solid blue;
+		/* padding: 10px; */
+		border: 1px solid #1a1a1a;
+		/* box-shadow: 0 0 2px #1a1a1a; */
 		background-color: #f9f9f9;
 		box-sizing: border-box;
 		display: flex;
@@ -258,9 +253,11 @@
 		justify-content: center;
 		align-items: center;
 	}
+	.gridSmallCard h3{
+		text-align: center;
+	}
 	.gridBigCard {
-		border: 0.51px solid blue;
-		padding: 10px;
+		border: 1px solid #1a1a1a;
 		grid-column: span 2;
 		background-color: #f9f9f9;
 		box-sizing: border-box;
@@ -274,7 +271,8 @@
 	}
 	.registerButton {
 		width: 100%;
-		height: 50px;
+		height: 100%;
+		min-height: 80px;
 		margin: 0;
 		border: none;
 		text-transform: uppercase;
@@ -285,10 +283,30 @@
 		cursor: pointer;
 		transition: all 0.25s;
 		padding: 10px;
+		text-decoration: underline;
 	}
 	.registerButton:hover {
 		background-color: blue;
 		color: #f9f9f9;
+	}
+
+	footer {
+		/* border: 0.5px solid blue; */
+		/* width: calc(100% - 19px); */
+		width: 100%;
+		max-width: 800px;
+		padding: 0 10px;
+		box-sizing: border-box;
+	}
+	.footerContainer {
+		/* border: 0.5px solid blue; */
+		background: #f9f9f9;
+		padding: 10px;
+		box-sizing: border-box;
+		color: #1a1a1a;
+		font-weight: 300;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
 	}
 
 	/* for Firefox */
@@ -325,13 +343,13 @@
 	}
 
 	@media screen and (max-width: 700px) {
-		.gridContainer {
+		/* .gridContainer {
 			display: grid;
 			grid-template-columns: 1fr;
-		}
-		.gridBigCard {
+		} */
+		/* .gridBigCard {
 			grid-column: span 1;
 			padding: 10px;
-		}
+		} */
 	}
 </style>
